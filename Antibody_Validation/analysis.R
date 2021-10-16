17c8a7f7785cd84bf5b12e12103546054589b8b2
Colby Ford <colbytylerford@gmail.com>
2021-10-16 09:47:24 -0400

    Update analysis.R

--- Antibody_Validation/analysis.R
+++ Antibody_Validation/analysis.R
@@ -16,7 +16,22 @@ CSP_data <- read_csv("../CSP_sequence_analysis/finalTab_csp.csv") %>%
   pivot_wider(names_from = "Haplotype", values_from = "Reads", values_fill = 0) %>% 
   mutate(SampleID = str_split(SampleID, "-", n = 2) %>% sapply(head, 1)) %>% 
   filter(SampleID %in% antibody_data$SampleID) %>% 
-  mutate(TotalReads = rowSums(select_if(., is.numeric), na.rm = TRUE))
+  # mutate(TotalReads = rowSums(select_if(., is.numeric), na.rm = TRUE))
+  gather(variable, value, -SampleID) %>% 
+  group_by(SampleID) %>% 
+  mutate(percentage = value/sum(value)) %>% 
+  select(-value) %>% 
+  spread(variable, percentage) %>% 
+  # ungroup(SampleID) %>% 
+  # mutate(TotalReads = rowSums(select_if(., is.numeric), na.rm = TRUE))
+
+
+
+gather(variable, value, -id) %>% 
+  group_by(id) %>% 
+  mutate(percentage = value/sum(value)) %>% 
+  select(-value) %>% 
+  spread(variable, percentage)
 
 
 HADDOCK_data <- read_xlsx("../CSP_sequence_analysis/protein/HADDOCK_Results.xlsx")
