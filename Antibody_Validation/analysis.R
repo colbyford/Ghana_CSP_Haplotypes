9518bb80d64a0524f63f268417686cd20bff948c
Colby Ford <colbytylerford@gmail.com>
2021-10-17 00:29:46 -0400

    Update analysis.R

--- Antibody_Validation/analysis.R
+++ Antibody_Validation/analysis.R
@@ -10,9 +10,44 @@ antibody_data <- read_xls("csp antibody data.xls", sheet = "Samples All") %>%
   select(SampleID, `Avg Conc`)
 
 
+## DESeq2 Prep
+library(DESeq2)
+CSP_data_orig <- read_csv("../CSP_sequence_analysis/finalTab_csp.csv") %>% 
+  select(SampleID, Haplotype, Reads) %>% 
+  filter(! Haplotype %in% c("Chimera", "Indels", "Noise", "Singelton")) %>% ## Only True Haplotypes
+  # mutate(Reads = Reads / exp(mean(log(Reads[Reads > 0])))) %>%  ## Geometric Mean Ratio 
+  pivot_wider(names_from = "Haplotype", values_from = "Reads", values_fill = 1) %>% 
+  mutate(SampleID = str_split(SampleID, "-", n = 2) %>% sapply(head, 1)) %>% 
+  filter(SampleID %in% antibody_data$SampleID)
+
+CSP_readcounts <- CSP_data_orig %>% 
+  select(-SampleID) %>% 
+  t() %>% data.frame()
+
+colnames(CSP_readcounts) <- CSP_data_orig$SampleID
+
+CSP_sampleinfo <- data.frame(row.names = names(CSP_readcounts)) %>% 
+  mutate(gene = "CSP")
+
+
+DESeq.ds <- DESeqDataSetFromMatrix(countData = CSP_readcounts,
+                                   colData   = CSP_sampleinfo,
+                                   design    = ~ 1)
+
+colSums(counts(DESeq.ds)) %>% barplot
+
+DESeq.ds <- estimateSizeFactors(DESeq.ds) # calculate SFs, add them to object
+plot( sizeFactors(DESeq.ds), colSums(counts(DESeq.ds)), # assess them
+      ylab = "library sizes", xlab = "size factors", cex = .6 )
+
+
+log.norm.counts <- log2(counts(DESeq.ds, normalized = TRUE))
+
+##########
 CSP_data <- read_csv("../CSP_sequence_analysis/finalTab_csp.csv") %>% 
   select(SampleID, Haplotype, Reads) %>% 
-  filter(! Haplotype %in% c("Chimera", "Indels", "Noise", "Singelton")) %>% 
+  filter(! Haplotype %in% c("Chimera", "Indels", "Noise", "Singelton")) %>% ## Only True Haplotypes
+  # mutate(Reads = Reads / exp(mean(log(Reads[Reads > 0])))) %>%  ## Geometric Mean Ratio 
   pivot_wider(names_from = "Haplotype", values_from = "Reads", values_fill = 0) %>% 
   mutate(SampleID = str_split(SampleID, "-", n = 2) %>% sapply(head, 1)) %>% 
   filter(SampleID %in% antibody_data$SampleID) %>% 
@@ -24,7 +59,8 @@ CSP_data <- read_csv("../CSP_sequence_analysis/finalTab_csp.csv") %>%
   spread(variable, percentage) %>% 
   ungroup(SampleID) %>% 
   # mutate(TotalReads = rowSums(select_if(., is.numeric), na.rm = TRUE))
-  pivot_longer(cols = starts_with("csp-"), names_to = "Haplotype", values_to = "expression")
+  pivot_longer(cols = starts_with("csp-"), names_to = "Haplotype", values_to = "expression") %>% 
+  filter(expression > 0)
 
 
 
@@ -41,8 +77,7 @@ HADDOCK_data <- read_xlsx("../CSP_sequence_analysis/protein/HADDOCK_Results.xlsx
 comparison_data <- CSP_data %>%
   inner_join(antibody_data, by = "SampleID") %>% 
   mutate(weighted_avg_conc = expression * as.numeric(`Avg Conc`)) %>% 
-  inner_join(HADDOCK_data, by = "Haplotype") %>% 
-  filter(expression > 0)
+  inner_join(HADDOCK_data, by = "Haplotype") 
 
 write_csv(comparison_data, "antibody_conc_HADDOCK_comparison.csv")
 
